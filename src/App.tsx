84cd08fe41240bbcc663728e690eaa1368fa8103
devferx <ferkki@hotmail.com>
2023-12-28 11:15:52 -0400

    Update ESLint and Prettier configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 export const App = () => {
-  return <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-
-}
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Hello
+      </button>
+    </main>
+  );
+};
