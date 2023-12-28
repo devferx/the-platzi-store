dcf953c2e105eb68eab334d91df50abe98f6e6c7
devferx <ferkki@hotmail.com>
2023-12-28 11:58:43 -0400

    Add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { RouterProvider } from "react-router-dom";
+
+import { TanStackProvider } from "@shared/plugins/TanStackProvider";
 import { router } from "./router";
 
 export const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <TanStackProvider>
+      <RouterProvider router={router} />;
+    </TanStackProvider>
+  );
 };
