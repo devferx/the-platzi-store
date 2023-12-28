7d4f6bf5d0439078b0e1c5e6ec54caff498c514a
devferx <ferkki@hotmail.com>
2023-12-28 15:03:03 -0400

    Add SkeletonCard component for loading state

--- src/domains/home/pages/index.tsx
+++ src/domains/home/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Card } from "@home/components/Card";
 import { Pagination } from "@home/components/Pagination";
+import { SkeletonCard } from "@home/components/SkeletonCard";
 import { usePagination } from "@home/hooks/usePagination";
 import { useProducts } from "@shared/hooks/useProducts";
 
@@ -9,19 +10,18 @@ export const Home = () => {
 
   return (
     <section className="container mx-auto">
-      {isLoading && <p>Loading...</p>}
+      <div className="mt-5 grid gap-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
+        {isLoading &&
+          new Array(10)
+            .fill(1)
+            .map((_, idx) => <SkeletonCard key={`sk-${idx}`} />)}
+        {products &&
+          products.map((product) => (
+            <Card key={product.id} product={product} />
+          ))}
+      </div>
 
-      {products && (
-        <>
-          <div className="mt-5 grid gap-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
-            {products.map((product) => (
-              <Card key={product.id} product={product} />
-            ))}
-          </div>
-
-          <Pagination page={page} nextPage={nextPage} prevPage={prevPage} />
-        </>
-      )}
+      <Pagination page={page} nextPage={nextPage} prevPage={prevPage} />
     </section>
   );
 };
