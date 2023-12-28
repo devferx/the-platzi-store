31a9080e4c6d658a907e79bce00c6fa63a5a0941
devferx <ferkki@hotmail.com>
2023-12-28 12:22:15 -0400

    Add store API, product model, and product service

---
+++ src/domains/shared/services/products.ts
@@ -0,0 +1,13 @@
+import { storeApi } from "@shared/api/store";
+import { Product } from "@shared/models/product";
+
+export async function getProducts(page = 1): Promise<Product[]> {
+  const offset = (page - 1) * 10;
+  const limit = 10;
+
+  const { data } = await storeApi.get<Product[]>(
+    `/products?offset=${offset}&limit=${limit}`,
+  );
+
+  return data;
+}
