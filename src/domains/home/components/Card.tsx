fb151e1189119b81fb12cf6e8b52561c41d05ec8
devferx <ferkki@hotmail.com>
2023-12-31 15:39:32 -0400

    Update add to cart link to button

--- src/domains/home/components/Card.tsx
+++ src/domains/home/components/Card.tsx
@@ -45,13 +45,12 @@ export const Card = ({ product }: CardProps) => {
           <span className="text-3xl font-bold text-gray-900 dark:text-white">
             ${product.price}
           </span>
-          <a
-            href="#"
+          <button
             className="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             onClick={handleAddToCart}
           >
             Add to cart
-          </a>
+          </button>
         </div>
       </div>
     </div>
